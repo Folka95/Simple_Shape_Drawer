b9ff9d945b15d7417396bd278c39ee298659bf3d
Marwan <marwanhussein2004@gmail.com>
2026-05-08 06:26:30 +0300

    refactor filling algorithm to use stack instead of vector for improved performance

--- algorithms/filling/iterive_flood_fill_filling_algorithm.cpp
+++ algorithms/filling/iterive_flood_fill_filling_algorithm.cpp
@@ -6,12 +6,12 @@ IterFloodFill_FillingAlgorithm::IterFloodFill_FillingAlgorithm() : FillingAlgori
 
 void IterFloodFill_FillingAlgorithm::fill(const Shape &shape, const Shape &clippingRegion, const Point &startPoint, ScreenWriter *sw) const {
     sw->activate();
-    std::vector<Point> stack;
+    std::stack<Point> stack;
     COLORREF fillableColor = sw->getPixel(startPoint.x, startPoint.y);
-    stack.push_back(startPoint);
+    stack.push(startPoint);
     while(!stack.empty()) {
-        Point current = stack.back();
-        stack.pop_back();
+        Point current = stack.top();
+        stack.pop();
 
         if(current.x < 0 || current.x >= sw->getWidth() || current.y < 0 || current.y >= sw->getHeight()) {
             continue;
@@ -21,10 +21,10 @@ void IterFloodFill_FillingAlgorithm::fill(const Shape &shape, const Shape &clipp
         if( shape.isInside(current) && currentColor == fillableColor &&
             currentColor != shape.borderColor && currentColor != shape.fillColor) {
             sw->setPixel(current.x, current.y, shape.fillColor);
-            stack.push_back(Point(current.x + 1, current.y));
-            stack.push_back(Point(current.x - 1, current.y));
-            stack.push_back(Point(current.x, current.y + 1));
-            stack.push_back(Point(current.x, current.y - 1));
+            stack.push(Point(current.x + 1, current.y));
+            stack.push(Point(current.x - 1, current.y));
+            stack.push(Point(current.x, current.y + 1));
+            stack.push(Point(current.x, current.y - 1));
         }
     }
     sw->deactivate();
