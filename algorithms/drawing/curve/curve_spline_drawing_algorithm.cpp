30721ffd76344cbfc37dcf61136d2b4211fba19b
Folka <mohamedkgm95@gmail.com>
2026-05-12 11:51:18 +0300

    - Restructure the curve spline drawing algorithm

--- algorithms/drawing/curve/curve_spline_drawing_algorithm.cpp
+++ algorithms/drawing/curve/curve_spline_drawing_algorithm.cpp
@@ -1,55 +1,50 @@
 #include "curve_spline_drawing_algorithm.h"
 
+
 Curve_Spline_DrawingAlgorithm::Curve_Spline_DrawingAlgorithm(int Cvalue) : DrawingAlgorithm() {
 	this->Cvalue = Cvalue / 100.0;
 }
 
-void Curve_Spline_DrawingAlgorithm::drawCurveHermite(Point t0, Point t1, Point p0, Point p3, ScreenWriter *sw, const Shape &shape) const {
+void Curve_Spline_DrawingAlgorithm::drawCurveHermite(Point t0, Point t1, Point p0, Point p3, ScreenWriter *sw, CurveShape* curve) const {
 	double xt;
 	double yt;
+	sw->activate();
 	for (double t = 0; t <= 1; t += 0.001) {
 		double h0 = 2 * pow(t, 3) - 3 * pow(t, 2) + 1;
 		double h1 = pow(t, 3) - 2 * pow(t, 2) + t;
 		double h2 = -2 * pow(t, 3) + 3 * pow(t, 2);
 		double h3 = pow(t, 3) - pow(t, 2);
-
 		xt = h0 * p0.x + h1 * t0.x + h2 * p3.x + h3 * t1.x;
 		yt = h0 * p0.y + h1 * t0.y + h2 * p3.y + h3 * t1.y;
+		sw->setPixel(round(xt), round(yt), curve->borderColor);
+	}
+	sw->deactivate();
+}
 
-		sw->setPixel(round(xt), round(yt), shape.borderColor);
+void Curve_Spline_DrawingAlgorithm::runAlgorithm(CurveShape* curve, ScreenWriter *sw) const {
+	double c1 = Cvalue;
+
+	Point T0;
+	T0.x = c1 * (curve->points[2].x - curve->points[0].x);
+	T0.y = c1 * (curve->points[2].y - curve->points[0].y);
+	for (int i = 2; i < curve->points.size() - 1; i++) {
+		Point T1;
+		T1.x = c1 * (curve->points[i + 1].x - curve->points[i - 1].x);
+		T1.y = c1 * (curve->points[i + 1].y - curve->points[i - 1].y);
+		drawCurveHermite(T0,T1,curve->points[i - 1],curve->points[i],sw, curve);
+		T0 = T1;
 	}
 }
 
-void Curve_Spline_DrawingAlgorithm::draw(const Shape &shape, ScreenWriter *sw) const
-{
-    if(shape.getType() != SHAPE_CURVE) {
-        std::cerr << "Curve_Spline_DrawingAlgorithm::draw : shape to draw must be Curve" << std::endl;
-        return;
-    }
-
-    if(shape.points.size() < 4) {
-        std::cerr << "Curve spline requires at least 4 points" << std::endl;
-        return;
-    }
-
-    double c1 = Cvalue;
-
-    sw->activate();
-
-    Point T0;
-    T0.x = c1 * (shape.points[2].x - shape.points[0].x);
-    T0.y = c1 * (shape.points[2].y - shape.points[0].y);
-
-    for (int i = 2; i < shape.points.size() - 1; i++) {
-        Point T1;
-
-        T1.x = c1 * (shape.points[i + 1].x - shape.points[i - 1].x);
-        T1.y = c1 * (shape.points[i + 1].y - shape.points[i - 1].y);
-
-        drawCurveHermite(T0,T1,shape.points[i - 1],shape.points[i],sw, shape);
-
-        T0 = T1;
-    }
-
-    sw->deactivate();
+void Curve_Spline_DrawingAlgorithm::draw(const Shape &inputShape, ScreenWriter *sw) const {
+	if(inputShape.getType() != SHAPE_CURVE) {
+		std::cerr << "Curve_Spline_DrawingAlgorithm::draw : shape to draw must be Curve" << std::endl;
+		return;
+	}
+	if(inputShape.getSize() < 4) {
+		std::cerr << "Curve spline requires at least 4 points" << std::endl;
+		return;
+	}
+	CurveShape* curve = dynamic_cast<CurveShape*>(inputShape.clone());
+	this->runAlgorithm(curve, sw);
 }
--- algorithms/drawing/curve/curve_spline_drawing_algorithm.h
+++ algorithms/drawing/curve/curve_spline_drawing_algorithm.h
@@ -2,12 +2,14 @@
 #ifndef CURVE_SPLINE_DRAWING_ALGORITHM_H
 #define CURVE_SPLINE_DRAWING_ALGORITHM_H
 #include "../../drawing_algorithm.h"
+#include "../../../core/shapes/curve.h"
 
 
 class Curve_Spline_DrawingAlgorithm : public DrawingAlgorithm {
 private:
     double Cvalue;
-    void drawCurveHermite(Point t0, Point t1, Point p0, Point p1, ScreenWriter *sw, const Shape &shape) const;
+    void drawCurveHermite(Point t0, Point t1, Point p0, Point p1, ScreenWriter *sw, CurveShape* curve) const;
+    void runAlgorithm(CurveShape* curve, ScreenWriter *sw) const ;
 public:
     Curve_Spline_DrawingAlgorithm(int Cvalue);
     void draw(const Shape &shape, ScreenWriter *sw) const;
